988da9d95e5a398c1318a6c487fefa7e85039507
DenilTalaviya7074 <deniltalaviya7074@gmail.com>
2026-07-19 09:30:43 +0530

    extractors created

---
+++ extractors.py
@@ -0,0 +1,5 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import List, Optional
